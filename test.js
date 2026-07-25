77fd5c5524ea1dded138fff824a6e824f7f0baef
Support WorldR <karansinghgurjarmorena@gmail.com>
2026-07-26 02:07:55 +0530

    chore: remove test scripts

--- test.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: './backend/.env' });
-const { db } = require('./backend/src/config/database');
-async function run() {
-  const companies = await db('companies').select('id', 'name');
-  console.log(companies.map(c => c.name));
-  process.exit(0);
-}
-run();
